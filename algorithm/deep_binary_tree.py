5582546dce4b3f77e121757cbeb2438e6aa53c18
weiyufeng <yufengwei0316@gmail.com>
2016-11-15 13:10:53 +0800

    binart tree

--- algorithm/deep_binary_tree.py
+++ algorithm/deep_binary_tree.py
@@ -1,35 +1,29 @@
 #! /usr/bin/python
 # coding:utf-8
 __author__ = 'weiyufeng'
-
 class Node(object):
-    def __init__(self, data, left = None, right = None):
-        self.data = data
+    def __init__(self, value , left = None, right = None):
         self.left = left
         self.right = right
+        self.value= value
 
-tree = Node(1, Node(3, Node(7, Node(0)), Node(6)), Node(2, Node(5), Node(4)))
+def deep_binart_tree(root):
+    if root is None:
+        return -1
+    print root.value
+    deep_binart_tree(root.left)
+    deep_binart_tree(root.right)
 
-def deep(root):
-    if not root:
-        return
-    print root.data
-    deep(root.left)
-    deep(root.right)
 
 def maxDepth(root):
     if not root:
         return 0
-    return max(maxDepth(root.left), maxDepth(root.right)) +1
+    return max(maxDepth(root.left), maxDepth(root.right)) + 1
 
-def isSameTree(p ,q):
-    if p == None and q == None:
-        return True
-    elif p and q:
-        return p.data == q.data and isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
 
+tree = Node(1, Node(3, Node(7, Node(0)), Node(6)), Node(2, Node(5), Node(4)))
 
 if __name__ == "__main__":
-    # deep(tree)
+    # deep_binart_tree(tree)
     l = maxDepth(tree)
     print l
