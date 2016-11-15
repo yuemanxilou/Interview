7258467f51435d96e2db3745af6b2dc3e53a9a94
weiyufeng <yufengwei0316@gmail.com>
2016-11-15 13:53:34 +0800

    quick sort

--- algorithm/quick_sort.py
+++ algorithm/quick_sort.py
@@ -2,39 +2,34 @@
 # coding:utf-8
 __author__ = 'Michael'
 
-def partition(alist, start, end):
-    pivot = alist[start]
+def partition(alsit, first, last):
+    if first > last:
+        return -1
 
-    first = start + 1
-    last = end
+    privot = alsit[first]
+    left = first + 1
+    right = last
 
-    done  = False
+    done = False
 
     while not done:
-        while first <= last  and alist[first] <= pivot:
-            first = first + 1
-        while first <= last and alist[last] >= pivot:
-            last = last -  1
-
-        if first > last:
+        while left <= right and alsit[left] <= privot:
+            left = left + 1
+        while left <= right and alsit[right] >= privot:
+            right = right - 1
+        if left > right:
             done = True
         else:
-            tmp = alist[first]
-            alist[first] = alist[last]
-            alist[last] = tmp
+            alsit[left], alsit[right] = alsit[right], alsit[left]
 
-    tmp = alist[start]
-    alist[start] = alist[last]
-    alist[last] = tmp
+    alsit[first], alsit[right] = alsit[right], alsit[first]
+    return right
 
-    return last
-
-def quick_sort(alist, start, end):
-    if start < end:
-        mid = partition(alist, start, end)
-        quick_sort(alist, start, mid-1)
-        quick_sort(alist, mid+1, end)
-
+def quick_sort(alist, first, last):
+    if first < last:
+        pos = partition(alist, first, last)
+        quick_sort(alist, first, pos-1)
+        quick_sort(alist, pos+1, last)
 
 if __name__ == '__main__':
     L = [5,6,78,9,0,-1,2,3,-65,12]
