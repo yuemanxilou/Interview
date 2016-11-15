92a45ca29dbbd69599ccba62d7fd78607f5ee274
weiyufeng <yufengwei0316@gmail.com>
2016-11-15 11:58:25 +0800

    bubblesort

--- algorithm/bubble_sort.py
+++ algorithm/bubble_sort.py
@@ -2,14 +2,16 @@
 # coding:utf-8
 __author__ = 'Michael'
 
-
-def bubbleSort(alist):
-    for i in xrange(len(alist)-1,0, -1):
+def bubblesort(alist):
+    if len(alist) == 0:
+        return -1
+    for i in xrange(len(alist)-1, 0, -1):
         for j in xrange(i):
             if alist[j] > alist[j+1]:
                 alist[j], alist[j+1] = alist[j+1], alist[j]
 
+
 if __name__  == '__main__':
     L = [5,6,78,9,0,-1,2,3,-65,12]
-    bubbleSort(L)
+    bubblesort(L)
     print L
