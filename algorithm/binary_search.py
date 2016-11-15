48f69fdada5818cb34a42c21476c1c12253bf63a
weiyufeng <yufengwei0316@gmail.com>
2016-11-15 11:37:10 +0800

    add new func

--- algorithm/binary_search.py
+++ algorithm/binary_search.py
@@ -5,15 +5,16 @@ __author__ = 'Michael'
 def binary_search(alist, start, end, value):
     if start > end:
         return -1
-    mid = (start + end)/2
-    if value < alist[mid]:
-        return binary_search(alist, start, mid-1, value)
-    elif value > alist[mid]:
-        return binary_search(alist, mid+1, end, value)
     else:
-        return mid
+        mid = (start + end)/2
+        if alist[mid] > value:
+            return binary_search(alist, start, mid -1, value)
+        elif alist[mid] < value:
+            return binary_search(alist, mid + 1, end, value)
+        else:
+            return mid
 
 if __name__ == "__main__":
     L = [-65, -1, 0, 2, 3, 5, 6, 9, 12, 78]
-    pos = binary_search(L, 0, len(L)-1, 12)
+    pos = binary_search(L, 0, len(L)-1, 78)
     print pos
---
+++ python/python_func.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python
+# coding:utf-8
+__author__ = 'Michael'
+
+alist = ['I','am', 'a', 'student']
+print '-'.join(alist)
+
+str = 'I-am-a-student'
+print str.split('-')
